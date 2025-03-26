e6e7df65cb451a45d6fcfae9019520858ebf12a5
MorganLee <30387079+MorganLee0906@users.noreply.github.com>
2025-03-26 17:26:21 +0800

    Organize files and dir.

--- add_category_by_GPT.py
+++
@@ -1,7 +0,0 @@
-import os
-import pandas as pd
-dir_path = 'data/five_years'
-for i in os.listdir(dir_path):
-    if i.endswith('label.csv'):
-        df = pd.read_csv(dir_path + '/' + i)
-        for qnum in df['NUMBER']:
